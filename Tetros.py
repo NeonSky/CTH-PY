6afcf0f5df0214f57c66346ff7730995f38574fd
vltmn <melkerveltman@gmail.com>
2017-08-22 09:36:58 +0200

    added maximum framerate

--- Tetros.py
+++ Tetros.py
@@ -9,21 +9,27 @@ from Shape import Shape
 #GameManager -> Controls score and general game logic like spawning shapes and removing them. Also rendering
 #Tetros -> Screen management
 
+
 gameManager = GameManager()
 
 background_colour = (0, 0, 0)
 (width, height) = (640, 640)
 
+target_framerate = 60
 screen = pg.display.set_mode((width, height))
 pg.display.set_caption('Tetros')
 screen.fill(background_colour)
 
+clock = pg.time.Clock()
 
 running = True
 while running:
+  gameManager.update()
   gameManager.draw(screen)
   pg.display.flip()
 
+  #pygame.time.Clock.tick() sets a maximum framerate for the screen to draw
+  clock.tick(target_framerate)
   for event in pg.event.get():
     if event.type == pg.QUIT:
       running = False
