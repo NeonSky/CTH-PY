72e20d08fb7eed5248a38ecd4e12907580a797c0
Jacob Eriksson <jacob.a.eriksson@gmail.com>
2017-08-22 09:58:15 +0200

    Fixed position i blocks

--- BlockMap.py
+++ BlockMap.py
@@ -5,19 +5,21 @@ class BlockMap:
     blocks = {}
 
     def __init__(self):
-        default_color = (255, 0, 0)
+        default_color = (0, 0, 0)
 
         for i in range(0, 20, 1):
-            self.blocks[i] = (Block((0, i), default_color),
-                              Block((1, i), default_color),
-                              Block((2, i), default_color),
-                              Block((3, i), default_color),
-                              Block((4, i), default_color),
-                              Block((5, i), default_color),
-                              Block((6, i), default_color),
-                              Block((7, i), default_color),
-                              Block((8, i), default_color),
-                              Block((9, i), default_color))
+            self.blocks[i] = (Block((0*Block.width, i*Block.width), default_color),
+                              Block((1*Block.width, i*Block.width), default_color),
+                              Block((2*Block.width, i*Block.width), default_color),
+                              Block((3*Block.width, i*Block.width), default_color),
+                              Block((4*Block.width, i*Block.width), default_color),
+                              Block((5*Block.width, i*Block.width), default_color),
+                              Block((6*Block.width, i*Block.width), default_color),
+                              Block((7*Block.width, i*Block.width), default_color),
+                              Block((8*Block.width, i*Block.width), default_color),
+                              Block((9*Block.width, i*Block.width), default_color))
+
+        self.blocks[10][0].applyBlock(Block((0, 0), (255, 0, 100)))
 
     def collides(self, shape, x_change, y_change):
         return True
